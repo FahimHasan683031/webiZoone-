5e27619e6e816e9ca4db59bf5a863669fef64dd6
rakib38324 <rakib3832@gmail.com>
2024-05-29 17:14:37 +0600

    setup project with hello world

---
+++ app/helloworld/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HelloWorldPage = () => {
+    return (
+        <div>
+              <p className="my-10 text-center">Hello World</p>
+        </div>
+    );
+};
+
+export default HelloWorldPage;
