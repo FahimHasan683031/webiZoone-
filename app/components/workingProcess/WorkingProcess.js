50e89155e92dd6d66b1d27006c7c4e670ce10186
MacBook Pro <naym100m@gmail.com>
2024-06-04 10:17:00 +0600

    updated a timeline aimation with content

--- app/components/workingProcess/WorkingProcess.js
+++ app/components/workingProcess/WorkingProcess.js
@@ -1,6 +1,6 @@
 "use client";
 import { useEffect, useRef, useState } from "react";
-import { FaRegCircle } from "react-icons/fa6";
+import { FaGgCircle, FaRegCircle } from "react-icons/fa6";
 import { motion, useAnimation } from "framer-motion";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
@@ -119,34 +119,58 @@ const WorkingProcess = () => {
   ];
 
   return (
-    <div className="h-[700px] md:h-[750px] lg:h-[850px] max-w-screen-md mx-auto mb-20 mt-32 px-5 lg:px-0">
-      <section ref={timelineRef}>
-        <div className="relative">
-          <motion.div
-            className="vertical-line w-[3.5px] bg-[#bc4a32] absolute top-0 left-[50%]"
-            initial={{ height: 0 }}
-            animate={controls}
-          />
-          {timelineData.map((item, index) => (
-            <motion.div
-              key={index}
-              className="timeline-item"
-              initial={{ opacity: 0, y: 50 }}
-              whileInView={{ opacity: 1, y: 0 }}
-              viewport={{ once: true, amount: 0.8 }}
-              transition={{ duration: 2 }}
-            >
-              <TimelineItem
-                title={item.title}
-                description={item.description}
-                index={index}
-                isScrollUp={isScrollUp}
-              />
-            </motion.div>
-          ))}
+    <>
+      <div className="relative">
+        <div className="w-[3.5px] bg-[#bc4a32] h-[22px] absolute -top-6 left-[50%]"></div>
+        <div className="grid grid-cols-3">
+          <FaGgCircle className="text-xl md:text-3xl " />
+          <h3 className="text-xl md:text-3xl lg:text-4xl font-bold text-slate-800 text-center">
+            Our Process
+          </h3>
+          <div className="text-right">
+            <button className="px-2 md:px-4 py-2 rounded-full bg-[#bc4a32] text-white text-xs md:text-sm">
+              GET A QUOTE
+            </button>
+          </div>
         </div>
-      </section>
-    </div>
+        <p className="text-center w-full md:w-4/5 mx-auto mt-7 mb-9 text-[15px] lg:text-base ">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+          mollitia, molestiae quas vel sint commodi repudiandae consequuntur
+          voluptatum laborum numquam blanditiis harum quisquam eius sed odit
+          fugiat iusto fuga praesentium optio, eaque rerum! Provident similique
+          accusantium nemo autem. Veritatis
+        </p>
+      </div>
+
+      <div className="h-[700px] md:h-[750px] lg:h-[850px] max-w-screen-md mx-auto mb-20 mt-32 px-5 lg:px-0">
+        <section ref={timelineRef}>
+          <div className="relative">
+            <motion.div
+              className="vertical-line w-[3.5px] bg-[#bc4a32] absolute top-0 left-[50%]"
+              initial={{ height: 0 }}
+              animate={controls}
+            />
+            {timelineData.map((item, index) => (
+              <motion.div
+                key={index}
+                className="timeline-item"
+                initial={{ opacity: 0, y: 50 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                viewport={{ once: true, amount: 0.8 }}
+                transition={{ duration: 2 }}
+              >
+                <TimelineItem
+                  title={item.title}
+                  description={item.description}
+                  index={index}
+                  isScrollUp={isScrollUp}
+                />
+              </motion.div>
+            ))}
+          </div>
+        </section>
+      </div>
+    </>
   );
 };
 
